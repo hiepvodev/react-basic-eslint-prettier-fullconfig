8fcbbf9fd16bde5abcd81b6b455382c5dca6fd5e
hiep <vanhiep2102@gmail.com>
2023-10-07 10:27:27 +0700

    change text

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <h1>Hello, I'm Hiep</h1>
+        <h1>Hello, I'm Harry</h1>
         <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
